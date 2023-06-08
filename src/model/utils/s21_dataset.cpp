a51295c29960111a50ae893f85737805d6fd7a57
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2023-06-08 19:04:39 +0300

    [init][utils] Adding dataset class

---
+++ src/model/utils/s21_dataset.cpp
@@ -0,0 +1,8 @@
+//
+// Created by dolax on 03.06.2023.
+//
+
+#include "s21_dataset.h"
+
+namespace s21 {
+} // s21
---
+++ src/model/utils/s21_dataset.h
@@ -0,0 +1,16 @@
+//
+// Created by dolax on 03.06.2023.
+//
+
+#ifndef SRC_S21_DATASET_H
+#define SRC_S21_DATASET_H
+
+namespace s21 {
+
+    class s21_dataset {
+
+    };
+
+} // s21
+
+#endif //SRC_S21_DATASET_H
